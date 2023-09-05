bf9d85066a9e702a07e85ce9590ac23c0e511ec1
yerti <yerti0818@gmail.com>
2023-09-05 15:15:59 -0700

    Modify file to add changes to the create function

--- console.py
+++ console.py
@@ -118,13 +118,34 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        
+        class_name, *params = args.split(" ")
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        
+        new_instance = HBNBCommand.classes[class_name]()
+
+        for param in params:
+            key, value = param.split("=")
+
+            if value.startswith('"') and value.endswith('"'):
+                value = value.replace('"','\\"').replace('_', " ")
+            elif "." in value:
+                try:
+                    value = float(value)
+                except ValueError:
+                    continue
+            else:
+                try:
+                    value = int(value)
+                except ValueError:
+                    continue
+            setattr(new_instance, key, value)
+
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
