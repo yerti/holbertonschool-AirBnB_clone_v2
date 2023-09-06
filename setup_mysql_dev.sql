0d8d35e0d741859c1f3c3b39d05fcd51cae12ede
jharanza <jharanza1986@gmail.com>
2023-09-06 09:30:39 -0700

    feat: Add commets to document the file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
+-- Create tha database and the user
+-- Grant all privileges to the user in the new databse
+-- Grant SELECT privileges to the user in the performance_schema database 
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
