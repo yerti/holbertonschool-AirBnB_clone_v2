3727a2bf8c24d206c82ccf38d5840de7a2259015
yerti <yerti0818@gmail.com>
2023-09-07 10:18:02 -0700

    Fix: create an environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-
+import os
 
 storage = FileStorage()
+
+""" Check the value of the environment variable"""
+storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
