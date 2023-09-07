f9a65631fff422e8cf73337ff865fac8f7279ba8
jharanza <jharanza1986@gmail.com>
2023-09-07 10:08:33 -0700

    feat: Modified files city and states to support database

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
