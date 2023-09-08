9b248b5842e29062c1c11ccea90e385225fb7c7c
yerti <yerti0818@gmail.com>
2023-09-08 06:16:39 -0700

    fix: remove class imports

--- console.py
+++ console.py
@@ -4,12 +4,8 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
-from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
