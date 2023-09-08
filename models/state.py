412f0903999ce30287365dfb551c8e2df76a5d19
yerti <yerti0818@gmail.com>
2023-09-08 06:14:38 -0700

    Fix: add a conditional to create the columns and environment add a decorator for the foreign column

--- models/state.py
+++ models/state.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import models
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("city", backref="state", cascade="all, delete")
+    else:
+        name = ""
+
+    """function that calculates and returns all cities related to this intanica"""
+    """The property decorator only allows access to the function as if it were just a read attribute."""
+    @property
+        def cities(self):
+            new_list_cities = []
+            for city in models.storage.all('City').values():
+                if city.state_id == self.id:
+                    new_list_cities.append(city)
+
+            return new_list_cities
