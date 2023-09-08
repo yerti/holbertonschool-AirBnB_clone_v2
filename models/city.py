3faf00820a596d000fe10e2f18aefe29376e13d9
yerti <yerti0818@gmail.com>
2023-09-08 06:13:03 -0700

    Fix: add a conditional to create the columns

--- models/city.py
+++ models/city.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
+import models
+from models.base_model import BaseModel,Base
+from sqlalchemy import Column, Integer, String, Foreignkey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
 
-    state_id = Column(String(60), ForeignKey(states.id))
-    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') = 'db':
+        state_id = Column(String(60), ForeignKey(states.id), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        name: = ''
+        state_id = ''
