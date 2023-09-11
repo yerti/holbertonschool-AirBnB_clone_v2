f3e18f998ec3174e7a81977102c6d19107cbe51f
Jharanza <jharanza1986@gmail.com>
2023-09-11 14:27:01 -0700

    feat: Add test to the console

---
+++ tests/test_console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+    Test Case for the console
+'''
+import unnitest
+from unittest.mock import patch
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import models
+
+
+class testConsole(unittest.TestCase):
+    ''' Unittests to test the console '''
+    
+    def test_emptyline(self):
+        ''' Test empty line '''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("\n")
+            sekf,assertEqual('', f.getvalue())
+
+    def test_quit(self):
+        """Test quit command """
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("quit")
+            self.assertEqual('', f.getvalue())
+
+if __name__ == '__main__':
+    unittest.main()
