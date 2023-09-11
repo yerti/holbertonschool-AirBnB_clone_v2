469e6083ac209a0e22225ffc1f883510210032c1
yerti <yerti0818@gmail.com>
2023-09-11 14:51:25 -0700

    fix: poner el nombre de la tabla

--- models/amenity.py
+++ models/amenity.py
@@ -6,11 +6,9 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-Base = declarative_base()
-
-
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity')
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                    back_populates="amenities")
