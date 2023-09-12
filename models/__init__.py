f5a61389a586a7aa02e44e71161c369d3869427d
yerti <yerti0818@gmail.com>
2023-09-12 07:22:56 -0700

    Fix: delete an extra import

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-import os
+from os import getenv
 
-storage = FileStorage()
 
 """ Check the value of the environment variable"""
-storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage
