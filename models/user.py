46bd395e979c58c53e101e9f40a70dbb1055be75
yerti <109113310+yerti@users.noreply.github.com>
2023-09-12 18:43:13 -0500

    Fix: modify a column

--- models/user.py
+++ models/user.py
@@ -16,8 +16,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-        places = relationship('Place', backref='user', cascade='delete')
-        reviews = relationship('Review', backref='user', cascade='delete')
+        places = relationship('Place', backref='user', cascade='all, delete')
+        reviews = relationship('Review', backref='user', cascade='all, delete')
 else:
     class User(BaseModel):
         email = ""
