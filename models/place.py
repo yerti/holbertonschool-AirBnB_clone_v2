b6e2e69b12ee152dc641b16d23a904ad1ccafba9
yerti <yerti0818@gmail.com>
2023-09-12 07:26:39 -0700

    Fix: add a conditional to compare if it is a db

--- models/place.py
+++ models/place.py
@@ -8,46 +8,56 @@ from models.review import Review
 
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenity = Table('place_amenity', Base.metadata,
-                        Column('place_id',
-                                String(60),
-                                ForeignKey('places.id'),
-                                primary_key=True,
-                                nullable=False),
-                        Column('amenity_id',
-                                String(60),
-                                ForeignKey('amenities.id'),
-                                primary_key=True,
-                                nullable=False))
+    place_amenity = Table('place_amenity', Base.metadata,
+                    Column('place_id',
+                            String(60),
+                            ForeignKey('places.id'),
+                            primary_key=True,
+                            nullable=False),
+                    Column('amenity_id',
+                            String(60),
+                            ForeignKey('amenities.id'),
+                            primary_key=True,
+                            nullable=False))
 
 
-class Place(BaseModel, Base):
+    class Place(BaseModel, Base):
+        __tablename__ = 'places'
 
-    __tablename__ = 'places'
-
-    """ A place to stay """
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        """ A place to stay """
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        amenity_ids = []
         review = 'Review'
         reviews = relationship(review, backref='place', cascade='all, delete')
-
         amenities = relationship("Amenity", secondary=place_amenity,
                                 back_populates="place_amenities",
                                 viewonly=False)
-    
-    ''' relationship with file storage '''
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+else:
+    class Place(BaseModel):
+
+        """We create public attributes"""
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+        ''' relationship with file storage '''
         @property
         def reviews(self):
             ''' Return all reviews '''
