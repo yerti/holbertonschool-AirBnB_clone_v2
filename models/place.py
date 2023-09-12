0417a983bf5ab8dc5cbd328759ecbd5d0f39b21a
Jharanza <jharanza1986@gmail.com>
2023-09-12 09:09:11 -0700

    feat: Modifying the visual presentation

--- models/place.py
+++ models/place.py
@@ -8,17 +8,17 @@ from models.review import Review
 
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                    Column('place_id',
-                            String(60),
-                            ForeignKey('places.id'),
-                            primary_key=True,
-                            nullable=False),
-                    Column('amenity_id',
-                            String(60),
-                            ForeignKey('amenities.id'),
-                            primary_key=True,
-                            nullable=False))
+    place_amenity = Table(
+            'place_amenity', Base.metadata,
+            Column(
+                'place_id', String(60), ForeignKey('places.id'),
+                primary_key=True, nullable=False
+                ),
+            Column(
+                'amenity_id', String(60), ForeignKey('amenities.id'),
+                primary_key=True, nullable=False
+                )
+            )
 
     class Place(BaseModel, Base):
         __tablename__ = 'places'
