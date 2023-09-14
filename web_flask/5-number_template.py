92a1625c88367712d6943d308a422d2d732da1a3
yerti <yerti0818@gamil.com>
2023-09-14 14:11:58 -0700

    Feat: file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ return a hello string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return a hbnb string """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ returns a string depending on the text they give us as an argument """
+    new_text = text.replace('_', ' ')
+    return f"C {new_text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """ returns a string depending on the text they give us as an argument """
+    new_text = text.replace('_', ' ')
+    return f"Python {new_text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+    else:
+        return "Not Found", 404
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', num=n)
+    else:
+        return "Not Found", 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
